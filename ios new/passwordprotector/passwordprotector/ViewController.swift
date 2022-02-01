a26719c8aa270015d7152fb6b9eb4ff9ba28fdf8
akhila2akhila <akhila2akhila@gmail.com>
2022-02-01 21:08:43 +0530

    string class

--- ios new/passwordprotector/passwordprotector/ViewController.swift
+++ ios new/passwordprotector/passwordprotector/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var password = "akhila"
+    let dollarSign = "\u{24}"
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("your password is - \(password)")
+        print("your new password is - ",password + "7")
+        password = password + " ak"
+        print(dollarSign)
+        print(password.capitalized)
         // Do any additional setup after loading the view.
     }
 
